eb64a6ac6555b51b058b9bb0e5aae18597f9971a
Mizuki Fujita <mizuki.fujita@oocl.com>
2019-10-13 22:35:07 +0900

    Added Flatten

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flaten {
 
@@ -12,7 +14,9 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        return Arrays.stream(this.array)
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
